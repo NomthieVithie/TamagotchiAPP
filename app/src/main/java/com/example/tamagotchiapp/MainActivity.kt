90743e63017c04071506fdd2980393326a9d9e25
NomthieVithie <toodeelicious@gmail.com>
2024-09-30 19:28:56 +0000

    Implement basic virtual pet interactions: feeding, cleaning, and playing

--- app/src/main/java/com/example/tamagotchiapp/MainActivity.kt
+++ app/src/main/java/com/example/tamagotchiapp/MainActivity.kt
@@ -8,10 +8,12 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)//Set the layout for this activity to activity_main.xml
 
-        val button = findViewById<Button>(R.id.button)
+
+        val button = findViewById<Button>(R.id.button)//find the button using its ID
         button.setOnClickListener {
+            //create intent to start MainActivity2
             val intent = Intent(this, MainActivity2::class.java) // You can omit the full package if in the same package
             startActivity(intent) // Start the new activity
         }
